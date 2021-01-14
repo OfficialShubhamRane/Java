7945a1b8e4c827a1718e7b3cbb97255052c47ad1
Shubham Rane <official.shubhamrane@gmail.com>
2021-01-14 16:13:53 -0500

    School managment system Final commit

--- Projects/SchoolManagmentSystem/Students.java
+++ Projects/SchoolManagmentSystem/Students.java
@@ -3,11 +3,12 @@ public class Students {
     private String sFirstName;
     private String sLastName;
     private String sId;
-    private String courses;
+    private String courses="";
     private int gradeYear;
     private int tuitionBalance;
     private static int courseFee = 600;    
     private static int ID = 1000;
+    private String coursEntered;
     
     /**
      * Ask for name and graduation year 
@@ -30,9 +31,45 @@ public class Students {
         ID++;
         this.sId = gradeYear + "" + ID;
     }
+
     //Enroll in courses
+    void enrollCourses(){
+    do{  
+        System.out.print("Enter course name to enroll or Q to quit: ");
+        Scanner sc = new Scanner(System.in);
+        
+        coursEntered = sc.nextLine();
+        if(!coursEntered.equals("Q")){
+            courses = courses + " " + coursEntered;
+            tuitionBalance += courseFee;
+        }else{
+            break;
+        }
+    }while(1 != 0);
+    }
 
     //View balance
+    void viewBalance(){
+        System.out.println(sFirstName +" "+ sLastName + "'s remaining balance is $"+tuitionBalance +".");
+    }
 
     //Pay tuition
+    void payTution(){
+        viewBalance();
+        System.out.print("Make payment of: $");
+        Scanner sc = new Scanner(System.in);
+        int payment = sc.nextInt();
+        tuitionBalance -= payment;
+        System.out.println("Thank you for your payment for $"+payment+".");
+        viewBalance();
+    }
+
+    void viewInfo(){
+        System.out.println("Student ID: " + sId);
+        System.out.println("Name: " + sFirstName +" "+ sLastName);
+        System.out.println("Conurses Enrolled: "+ courses+".");
+        System.out.println(sFirstName +" "+ sLastName + "'s remaining balance is $"+tuitionBalance +".");
+
+    }
+
 }
