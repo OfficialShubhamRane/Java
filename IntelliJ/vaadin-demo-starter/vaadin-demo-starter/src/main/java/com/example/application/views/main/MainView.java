0904d90f496f0f8bee9b45c4849d045869c60b04
Shubham Rane <official.shubhamrane@gmail.com>
2021-04-16 17:35:18 -0400

    Vaadin-CRM-Tutorial Part 6 done

--- IntelliJ/vaadin-demo-starter/vaadin-demo-starter/src/main/java/com/example/application/views/main/MainView.java
+++ IntelliJ/vaadin-demo-starter/vaadin-demo-starter/src/main/java/com/example/application/views/main/MainView.java
@@ -1,16 +1,49 @@
 package com.example.application.views.main;
 
+import com.example.application.backend.entity.Company;
+import com.example.application.backend.entity.Contact;
+import com.example.application.backend.service.ContactService;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-/**
- * The main view is a top-level placeholder for other views.
- */
+
 @Route("/")
 public class MainView extends VerticalLayout {
 
-    public MainView() {
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    private ContactService contactService;
 
+    public MainView(ContactService contactService) {
+        this.contactService = contactService;
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+
+        add(grid);
+        updateList();
+
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.removeColumnByKey("company");
+
+        grid.setColumns("firstName", "lastName", "email", "status");
+        grid.addColumn(contact -> {
+            Company company = contact.getCompany();
+            return company == null ? "-" : company.getName();
+        }).setHeader("Company");
+
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+
+    }
+
+
+    private void updateList() {
+        grid.setItems(contactService.findAll());
     }
 
 }
