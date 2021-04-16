5f436404c28c88cc6652952afeffd1d9b76e9241
Shubham Rane <official.shubhamrane@gmail.com>
2021-04-16 19:33:02 -0400

    Vaadin CRM Tutorial part 10 Done

--- IntelliJ/vaadin-demo-starter/vaadin-demo-starter/src/main/java/com/example/application/views/main/MainView.java
+++ IntelliJ/vaadin-demo-starter/vaadin-demo-starter/src/main/java/com/example/application/views/main/MainView.java
@@ -4,9 +4,11 @@ import com.example.application.backend.entity.Company;
 import com.example.application.backend.entity.Contact;
 import com.example.application.backend.service.CompanyService;
 import com.example.application.backend.service.ContactService;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -26,20 +28,77 @@ public class MainView extends VerticalLayout {
     public MainView(ContactService contactService,
                     CompanyService companyService) {
         this.contactService = contactService;
+
         addClassName("list-view");
         setSizeFull();
         configureGrid();
-        configurationFilter();
+        getToolBar();
 
         form = new ContactForm(companyService.findAll());
+        form.addListener(ContactForm.SaveEvent.class, this::saveContact);
+        form.addListener(ContactForm.DeleteEvent.class, this::deleteContact);
+        form.addListener(ContactForm.CloseEvent.class, e ->closeEditor());
 
         Div content = new Div(grid, form);
         content.addClassName("content");
         content.setSizeFull();
 
-        add(filterText, content);
+        add(getToolBar(), content);
         updateList();
+        closeEditor();
 
+    }
+
+    private void deleteContact(ContactForm.DeleteEvent evt) {
+        contactService.delete(evt.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    private void saveContact(ContactForm.SaveEvent evt) {
+
+        contactService.save(evt.getContact());
+        updateList();
+        closeEditor();
+
+    }
+
+
+    private void editContact(Contact contact) {
+        if(contact == null){
+            closeEditor();
+        }else{
+            form.setContact(contact);
+            form.setVisible(true);
+            addClassName("editing");
+        }
+    }
+
+    private void closeEditor() {
+        form.setContact(null);
+        form.setVisible(false);
+        removeClassName("editing");
+    }
+
+    private void updateList() {
+        grid.setItems(contactService.findAll( filterText.getValue() ));
+    }
+
+    private HorizontalLayout getToolBar() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
+
+        Button addContactButton = new Button("Add contact", click -> addContact());
+        HorizontalLayout toolBar = new HorizontalLayout(filterText, addContactButton);
+        toolBar.addClassName("toolBar");
+        return toolBar;
+    }
+
+    private void addContact() {
+        grid.asSingleSelect().clear();
+        editContact(new Contact());
     }
 
     private void configureGrid() {
@@ -55,17 +114,7 @@ public class MainView extends VerticalLayout {
 
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
 
+        grid.asSingleSelect().addValueChangeListener(evt -> editContact(evt.getValue()));
 
-    }
-
-    private void updateList() {
-        grid.setItems(contactService.findAll( filterText.getValue() ));
-    }
-
-    private void configurationFilter() {
-        filterText.setPlaceholder("Filter by name...");
-        filterText.setClearButtonVisible(true);
-        filterText.setValueChangeMode(ValueChangeMode.LAZY);
-        filterText.addValueChangeListener(e -> updateList());
     }
 }
