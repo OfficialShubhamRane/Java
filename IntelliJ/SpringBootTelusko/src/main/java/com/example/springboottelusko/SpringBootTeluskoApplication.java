60c657ddb2acaac1aad9b1be70b13bd7d652812c
Shubham Rane <official.shubhamrane@gmail.com>
2021-04-28 14:07:57 -0400

    Spring Boot Telusko

---
+++ IntelliJ/SpringBootTelusko/src/main/java/com/example/springboottelusko/SpringBootTeluskoApplication.java
@@ -0,0 +1,18 @@
+package com.example.springboottelusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringBootTeluskoApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootTeluskoApplication.class, args);
+
+        Alien a1 = context.getBean(Alien.class);
+        a1.show();
+
+    }
+
+}
