6993328baa0906ebbd97b76bb0f1a1ff83d0744c
Shubham Rane <official.shubhamrane@gmail.com>
2021-05-07 19:41:37 -0400

    Started with fetching meaningful data from csv

--- IntelliJ/Covid19TrackerApplication/src/main/java/com/example/covid19trackerapplication/Covid19TrackerApplication.java
+++ IntelliJ/Covid19TrackerApplication/src/main/java/com/example/covid19trackerapplication/Covid19TrackerApplication.java
@@ -2,8 +2,10 @@ package com.example.covid19trackerapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Covid19TrackerApplication {
 
     public static void main(String[] args) {
