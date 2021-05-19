8c546fa7f084407f931331dd06d24253fc5ab29d
Shubham Rane <official.shubhamrane@gmail.com>
2021-05-19 12:59:26 -0400

    Practice Linked List

---
+++ Data Structures/LinkedLists/Singly_Node.java
@@ -0,0 +1,14 @@
+public class Singly_Node {
+
+    String data;
+    Singly_Node next;
+
+    /** Parameterized Constructor */
+    public Singly_Node(String data) {
+
+        this.data = data;
+        this.next = null;
+
+    }
+
+}
