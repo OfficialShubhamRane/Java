adf2f29af46d0a34614985b33aca4fbd607b46d3
Shubham Rane <official.shubhamrane@gmail.com>
2021-07-08 16:12:56 -0400

    Application form with JSP

--- IntelliJ/JSP_Tutorial/src/main/java/com/example/jsp_tutorial/controller/HomeController.java
+++ IntelliJ/JSP_Tutorial/src/main/java/com/example/jsp_tutorial/controller/HomeController.java
@@ -4,16 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @GetMapping({"/","/hello"})
+    @GetMapping("/")
     public String getHello(@RequestParam(value = "name", defaultValue = "world", required = true)
-                                       String name,
+                                        String name,
                                         Model model){
         System.out.println("Inside the getHello");
-        model.addAttribute("name" , name);
-        return "hello";
+        return "index";
+    }
+
+    @GetMapping("/register")
+    @ResponseBody
+    public String registerApplication(){
+        System.out.println("In the register");
+        return "Successfully registered";
     }
 }
--- IntelliJ/JSP_Tutorial/src/main/webapp/WEB-INF/jsp/index.jsp
+++ IntelliJ/JSP_Tutorial/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,6 +1,5 @@
-<jsp:useBean id="name" scope="request" type="java.lang.String"/>
-
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1" %>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +7,9 @@
     <title>Title</title>
 </head>
 <body>
-    <h2 align="center">Hello ${name}!</h2>
+
+    <h2 align="center"> Welcome to spring boot application form</h2>
+    <a href="${pageContext.request.contextPath}/register" >Click here to register</a>
+
 </body>
 </html>
