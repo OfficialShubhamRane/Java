ec00ebe7406ef1bcb02c5cfa6fcfc069bf937105
Shubham Rane <official.shubhamrane@gmail.com>
2021-07-14 20:02:42 -0400

    Spring security Authorization using HTTP Security

--- IntelliJ/SpringBoot_Basics/SpringSecurity/src/main/java/com/example/springsecurity/SpringSecurityApplication.java
+++ IntelliJ/SpringBoot_Basics/SpringSecurity/src/main/java/com/example/springsecurity/SpringSecurityApplication.java
@@ -22,4 +22,15 @@ class HelloWorld{
     public String getHelloWorld(){
         return "<h1>Hello World</h1>";
     }
+
+    @GetMapping("/user")
+    public String getUser(){
+        return "<h1>Hello User</h1>";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin(){
+        return "<h1>Hello Admin</h1>";
+    }
+
 }
