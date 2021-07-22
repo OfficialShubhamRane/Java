d3ae2d9b95219fb4c5f5ce8c8a0de5515c5de3e6
Shubham Rane <official.shubhamrane@gmail.com>
2021-07-22 12:22:16 -0400

    LL deletion

--- DataStructures/LinkedLists/LL_refresher.java
+++ DataStructures/LinkedLists/LL_refresher.java
@@ -1,5 +1,3 @@
-import java.net.Socket;
-
 public class LL_refresher {
 
     public static void main(String[] args) {
@@ -7,18 +5,37 @@ public class LL_refresher {
         Node head = new Node(10);
         Node temp_head = head;
 
-        int i = 0;
-        while (head.data <= 100) {
-            // System.out.println(i);
-            head.next = new Node(head.data + 10);
-            // System.out.println(head.data);
-            head = head.next;
-            i++;
-        }
-
-        while (temp_head.next != null) {
-            System.out.println(temp_head.data);
+        // SinglyLL insertion
+        while (temp_head.data <= 100) {
+            temp_head.next = new Node(temp_head.data + 10);
             temp_head = temp_head.next;
         }
+        temp_head = head; // reseting temp_head for future operations and reuse
+
+        // // Traversal
+        // while (head.next != null) {
+        // System.out.println(head.data);
+        // head = head.next;
+        // }
+
+        // SignlyLL deletion
+        int target = 40;
+        while (temp_head.next != null) {
+
+            if (temp_head.next.data == target) {
+                temp_head.next = temp_head.next.next;
+                break;
+            }
+            // System.out.println(temp_head.data);
+            temp_head = temp_head.next;
+
+        }
+
+        // Traversal
+        while (head.next != null) {
+            System.out.println(head.data);
+            head = head.next;
+        }
+
     }
 }
