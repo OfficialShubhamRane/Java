2ec718d117e3171ad8f72da03633d90165ed2e40
Shubham Rane <official.shubhamrane@gmail.com>
2022-01-23 12:14:14 -0600

    Added IOTestAutomator in its own directorty

---
+++ Projects/IO_Check_Automation/IOTestAutomator.java
@@ -0,0 +1,21 @@
+/**
+ * IOTestAutomator
+ */
+public class IOTestAutomator {
+
+    /** Read io.pl get the IPs of IBEs*/
+
+    /** Write those IPs in .csv */
+
+    /** Read .csv and ping to those IPs */
+
+    /** Read the response and note it down in a seperate .csv file
+     * format can be 
+     * "127.0.0.1,good,127.0.0.2,bad,....."
+     */
+
+     /** Generate good, easy to read UI from that file
+      * Maybe a green light red light stuff
+      */
+
+}
